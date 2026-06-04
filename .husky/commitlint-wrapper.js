ffc6184d7316e8a9bbb9b19553da59b7024a2308
Parmesh Bhatt <parmesh@weetechsolution.com>
2026-06-04 18:00:51 +0530

    refactor: remove old wrapper, add @commitlint/lint, update hooks

--- .husky/commitlint-wrapper.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync(process.argv[2], 'utf-8');
-const firstLine = content.split('\n')[0];
-if (firstLine.length > 100) {
-  console.log('✖ header must not be longer than 100 characters, current length is ' + firstLine.length + ' [header-max-length]');
-  console.log('✖ found 1 problems, 0 warnings');
-  process.exit(1);
-}
-process.exit(0);
